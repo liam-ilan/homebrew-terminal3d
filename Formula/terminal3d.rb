d3148d54bd17b36287578d899bd6412ebc9ed396
Liam Ilan <snowboardsheep@gmail.com>
2024-11-11 15:01:26 -0800

    v0.0.3

--- Formula/terminal3d.rb
+++ Formula/terminal3d.rb
@@ -1,8 +1,8 @@
 class Terminal3d < Formula
   desc "View 3d models in your terminal"
   homepage "https://github.com/liam-ilan/terminal3d"
-  url "https://github.com/liam-ilan/terminal3d/archive/refs/tags/v0.0.2.tar.gz"
-  sha256 "e932b9033c8173265135ebcf4a35c1e680737079e7db7ec85c71e433b3cd2170"
+  url "https://github.com/liam-ilan/terminal3d/archive/refs/tags/v0.0.3.tar.gz"
+  sha256 "2f8f50a40fd3f454a5c640cbb8dd6610aa51c37629cc2f9e52a950aa9db8c5f0"
   license "MIT"
 
   depends_on "rust" => :build
